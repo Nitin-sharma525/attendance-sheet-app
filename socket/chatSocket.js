66dddd0da64332b39ee8e04edbdf5ef2ef90d872
Nitin Sharma <nitinzeroit@gmail.com>
2025-11-18 17:45:08 +0530

    update the socket

--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -3,8 +3,6 @@ module.exports = (io, socket) => {
   const senderUUID = user.id;
 
   socket.emit("system", `Welcome ${user.email}`);
-
-  
   socket.join(senderUUID);
 
   
@@ -17,13 +15,46 @@ module.exports = (io, socket) => {
       senderUUID,
       receiverUUID: data.receiverUUID,
       message: data.message || "",
+      fileName: data.fileName || null,
+      fileType: data.fileType || null,
+      fileData: data.fileData || null, 
+      isChunked: false,
       time: new Date().toISOString(),
     };
 
-    
     io.to(data.receiverUUID).emit("receiveMessage", msg);
-    io.to(senderUUID).emit("receiveMessage", msg); 
+    io.to(senderUUID).emit("receiveMessage", msg);
 
-    console.log("Message Sent:", msg);
+    console.log("Standard Message Sent:", msg.fileName || msg.message);
+  });
+
+  
+  const fileChunks = {};
+
+  socket.on("sendChunk", (data) => {
+    const { receiverUUID, fileName, fileType, chunk, isLast } = data;
+
+    if (!fileChunks[fileName]) fileChunks[fileName] = [];
+    fileChunks[fileName].push(chunk);
+
+    if (isLast) {
+      const finalBuffer = Buffer.concat(fileChunks[fileName]);
+      delete fileChunks[fileName];
+
+      const msg = {
+        senderUUID,
+        receiverUUID,
+        fileName,
+        fileType,
+        fileData: finalBuffer,
+        isChunked: true,
+        time: new Date().toISOString(),
+      };
+
+      io.to(receiverUUID).emit("receiveChunkedFile", msg);
+      io.to(senderUUID).emit("receiveChunkedFile", msg);
+
+      console.log("Chunked File Finalized:", fileName);
+    }
   });
 };
